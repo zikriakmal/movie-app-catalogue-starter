816b7f0ee770a10e372af0fe47e32182fd0cecad
Zikri Akmal S <zikriakmale@gmail.com>
2024-07-19 09:17:15 +0700

    init: initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+const colors = {
+    primary: "#110E47",
+    secondary: "#88A4E8",
+    secondaryLight: "#DBE3FF",
+    grey: "#AAA9B1",
+    greyLight: "#E5E4EA",
+    black: "#110E47",
+    white: "#FFFFFF",
+}
+
+export { colors }
